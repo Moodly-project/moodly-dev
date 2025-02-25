dc25efd41841e9c19b6251e0a3b2d1e864fc221d
Rodrigo <rodrigo.teixeira@sou.unifeob.edu.br>
2025-02-24 21:02:01 -0300

    Removidos comentários

--- moodly_dart/lib/main.dart
+++ moodly_dart/lib/main.dart
@@ -10,7 +10,7 @@ class MoodlyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false, // Remove a faixa de debug
+      debugShowCheckedModeBanner: false,
       home: SplashScreen(),
     );
   }
@@ -32,21 +32,21 @@ class _SplashScreenState extends State<SplashScreen>
   void initState() {
     super.initState();
 
-    // Configura o controlador da animação
+
     _controller = AnimationController(
-      duration: const Duration(seconds: 2), // Duração da animação
+      duration: const Duration(seconds: 2),
       vsync: this,
     );
 
-    // Define a animação de escala (a logo vai crescer e depois encolher)
+    
     _scaleAnimation = Tween<double>(begin: 0.5, end: 1.2).animate(
       CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
     );
 
-    // Inicia a animação automaticamente
+    
     _controller.forward().then((_) {
       _controller.reverse().then((_) {
-        // Após a animação terminar, navega para a próxima tela (ainda não implementada)
+
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(builder: (context) => const NextScreen()),
@@ -57,27 +57,27 @@ class _SplashScreenState extends State<SplashScreen>
 
   @override
   void dispose() {
-    _controller.dispose(); // Libera o controlador ao sair da tela
+    _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white, // Cor de fundo da tela
+      backgroundColor: Colors.white, 
       body: Center(
         child: AnimatedBuilder(
           animation: _controller,
           builder: (context, child) {
             return Transform.scale(
-              scale: _scaleAnimation.value, // Aplica a animação de escala
+              scale: _scaleAnimation.value,
               child: child,
             );
           },
           child: Image.asset(
-            'assets/logo.png', // Substitua pelo caminho da sua logo
-            width: 150, // Tamanho base da logo
-            height: 150,
+            'assets/logo.png',
+            width: 330,
+            height: 330,
           ),
         ),
       ),
@@ -85,7 +85,7 @@ class _SplashScreenState extends State<SplashScreen>
   }
 }
 
-// Placeholder para a próxima tela (será substituído depois)
+
 class NextScreen extends StatelessWidget {
   const NextScreen({super.key});
 
